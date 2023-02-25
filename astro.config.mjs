cb4c048a900b63706fc0a35a9b2e40cfc57b7f5e
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2023-02-25 09:56:11 +0530

    feat: add prefetch integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://krishna-santosh.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), prefetch()]
 });
